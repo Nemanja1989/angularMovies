bcbada46abc44776e299f4dd9a6efb624397ba2e
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-01-25 18:32:06 +0100

    master navbar,router,first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './layout/layout.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {MoviesComponent} from './components/movies/movies.component';
 
+const appRoutes: Routes = [
+    {path: '', component: MoviesComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LayoutComponent,
+        NavbarComponent,
+        MoviesComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
